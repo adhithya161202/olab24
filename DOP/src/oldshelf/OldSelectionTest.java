a2a49f87cfd72b3c63749b058c09369b011235d7
adhithya161202 <adhithyaofficial0113@gmail.com>
2024-11-03 19:42:29 +0530

    new6

---
+++ DOP/src/oldshelf/OldSelectionTest.java
@@ -0,0 +1,19 @@
+package oldshelf;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+class OldSelectionTest {
+
+    @Test
+    void testGetAgeOrTitle_Comic() {
+        Comic comic = new Comic("Batman", 50);
+        assertEquals("superman", OldSelection.getAgeOrTitle(comic));
+    }
+
+    @Test
+    void testGetAgeOrTitle_SomeObject() {
+        Object unrelatedObject = new Object();
+        assertEquals("", OldSelection.getAgeOrTitle(SomeObject));
+    }
+}
